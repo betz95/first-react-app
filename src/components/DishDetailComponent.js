dd99f717e07060ca3c68800c883ac0f3b6b78b12
betz95 <yudiono.albert@gmail.com>
2018-11-17 21:31:49 +0700

    presentational and container components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
-        const dish = this.props.selectedDish;
+        const dish = this.props.dish;
         if (dish != null) {
-            const dishComments = this.props.selectedDish.comments.map((dishComment) => {
+            const dishComments = dish.comments.map((dishComment) => {
                 return (
-                    <div key={dishComment.id}>
-                        <p>{dishComment.comment}</p>
-                        <p>--{dishComment.author}, {dishComment.date}</p>
-                    </div>
+                        <div key={dishComment.id}>
+                            <p>{dishComment.comment}</p>
+                            <p>--{dishComment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(dishComment.date)))}</p>
+                        </div>
                 )
             });
 
             return (
-                <div className="col-12">
+                <div className="container">
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
                             <Card>
