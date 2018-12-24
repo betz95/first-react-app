3dab9cc94db8baa7522635468467369cef85a019
betz95 <yudiono.albert@gmail.com>
2018-12-24 22:42:27 +0700

    fetch post comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -21,7 +21,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
     if (comments != null) {
         return (
             <div className="col-12 col-md-5 m-1">
@@ -36,7 +36,7 @@ function RenderComments({comments, addComment, dishId}) {
                         )
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
     }
@@ -75,7 +75,7 @@ const DishDetail = (props) => {
                 <div className="row">
                     <RenderDish dish={props.dish} />
                     <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id} />
                 </div>
             </div>
