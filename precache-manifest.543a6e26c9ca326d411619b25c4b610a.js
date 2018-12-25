d82e979b4d214b43118962c59628744d77d683d2
betz95 <yudiono.albert@gmail.com>
2018-12-25 15:27:52 +0700

    Updates

---
+++ precache-manifest.543a6e26c9ca326d411619b25c4b610a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed9d9ecfc77bef7f316a",
+    "url": "/first-react-app/static/css/main.555f38c2.chunk.css"
+  },
+  {
+    "revision": "ed9d9ecfc77bef7f316a",
+    "url": "/first-react-app/static/js/main.ed9d9ecf.chunk.js"
+  },
+  {
+    "revision": "125ab3cabc3a90a9245f",
+    "url": "/first-react-app/static/css/1.6d206858.chunk.css"
+  },
+  {
+    "revision": "125ab3cabc3a90a9245f",
+    "url": "/first-react-app/static/js/1.125ab3ca.chunk.js"
+  },
+  {
+    "revision": "77868f616b63da4b2fc7",
+    "url": "/first-react-app/static/js/runtime~main.77868f61.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/first-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/first-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/first-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/first-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/first-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ff55c145681a3d3a950fa2e42b2fe62b",
+    "url": "/first-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/first-react-app/src/precache-manifest.1a718f0d2751c2a748134729d91ac2e7.js"
+  "/first-react-app/precache-manifest.543a6e26c9ca326d411619b25c4b610a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/first-react-app/src/index.html", {
+workbox.routing.registerNavigationRoute("/first-react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
