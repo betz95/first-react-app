c3243032a6ff7cd5d3e742c4158d626fbe87c9f4
betz95 <yudiono.albert@gmail.com>
2018-12-25 15:24:30 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/first-react-app/precache-manifest.8ac404969d910b48e43e2171b983665d.js"
+  "/first-react-app/src/precache-manifest.1a718f0d2751c2a748134729d91ac2e7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/first-react-app/index.html", {
+workbox.routing.registerNavigationRoute("/first-react-app/src/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
